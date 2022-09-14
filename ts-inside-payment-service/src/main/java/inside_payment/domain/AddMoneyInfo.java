f9cc6f98bfaf3daf6970770c9b871605abc59de5
Ans Nadeem <ans_nadeem@outlook.com>
2022-09-14 16:50:15 -0500

    Vanilla version of train ticket

---
+++ ts-inside-payment-service/src/main/java/inside_payment/domain/AddMoneyInfo.java
@@ -0,0 +1,26 @@
+package inside_payment.domain;
+
+public class AddMoneyInfo {
+    private String userId;
+    private String money;
+
+
+    public AddMoneyInfo(){}
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+}
---
+++ ts-rebook-service/src/main/java/rebook/domain/AddMoneyInfo.java
@@ -0,0 +1,33 @@
+package rebook.domain;
+
+
+
+
+
+public class AddMoneyInfo {
+
+    private String userId;
+
+    private String money;
+
+    public AddMoneyInfo(){
+        //Default Constructor
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+}
